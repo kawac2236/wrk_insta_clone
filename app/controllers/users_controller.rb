a2f4bfe0cd436cfab308e328b515a1cad2e946c6
kawac2236 <kawac2236@gmail.com>
2019-10-16 22:13:37 +0900

    最低限のユーザー登録フォームの作成（名前とメールアドレス）、ユーザー詳細はスタブを作成。debug用に debug(param)を追加。 #2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,27 @@
 class UsersController < ApplicationController
-  def new; end
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 成功したとき
+      render :new
+    else
+      # 失敗したとき
+      render :new
+    end
+
+    render :new
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name,:email)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_132539) do
+ActiveRecord::Schema.define(version: 2019_10_16_111517) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
