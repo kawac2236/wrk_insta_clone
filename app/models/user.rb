ebaf285c4f0538cf82028429a6ca11d9a22662f1
kawac2236 <kawac2236@gmail.com>
2019-10-18 06:20:22 +0900

    fix: form_withでデフォルトのJSレンダリング→HTMLレンダリングに変更。記載ミスを修正。 #2

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
   # sorcery
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
