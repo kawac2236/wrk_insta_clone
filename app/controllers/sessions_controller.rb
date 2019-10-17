3cb1e4b52ad70e3e25e17e7f41751bcbd869d65d
kawac2236 <kawac2236@gmail.com>
2019-10-18 08:09:59 +0900

    refactor: flashのパーシャル化。メッセージまわりのスタイルを整理。 #2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,24 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # user = User.find_by(email: session_params[:email])
-    # binding.pry
+    # Takes credentials and returns a user on successful authentication.
     @user = login(params[:email], params[:password])
 
     if @user
-      # binding.pry
       redirect_back_or_to root_path, success:'ログインしました'
     else
-      # binding.pry
-      flash.now[:alert] = 'ログインに失敗しました'
+      flash.now[:danger] = 'ログインに失敗しました'
       render :new
     end
   end
 
   def destroy
-    # session情報は全破棄
-    reset_session
-    redirect_to root_url, notice: 'ログアウトしました'
+    # Resets the session and runs hooks before and after.
+    logout
+    redirect_to root_url, info: 'ログアウトしました'
   end
 
 end
