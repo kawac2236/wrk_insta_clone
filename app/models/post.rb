447d6853fc5a7334e460f43cf9b1978c5ad9f093
kawac2236 <kawac2236@gmail.com>
2019-10-22 18:50:17 +0900

    add: 投稿の制限、辞書の追加 #4

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,19 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  content    :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#  images     :json
+#
+
 class Post < ApplicationRecord
   mount_uploaders :images, PostImageUploader
   belongs_to :user
+
+  validates :content, presence: true, length: { maximum: 1000 }
+  validates :images, presence: true
 end
