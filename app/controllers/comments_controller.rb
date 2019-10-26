6ca4231a69b5f698f3eb50d4f73a06ff525c0aeb
kawac2236 <kawac2236@gmail.com>
2019-10-26 21:07:27 +0900

    add: コメント編集の追加（モーダルの挿入）

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,9 +11,23 @@ class CommentsController < ApplicationController
     @comment = current_user.comments.find(params[:id])
     @comment.destroy!
   end
+
+  def edit
+    @comment = current_user.comments.find(params[:id])
+  end
+
+  def update
+    @comment = current_user.comments.find(params[:id])
+    @comment.update(comment_update_params)
+  end
+
   private
 
   def comment_params
     params.require(:comment).permit(:content).merge(post_id: params[:post_id])
+  end
+
+  def comment_update_params
+    params.require(:comment).permit(:content)
   end
 end
