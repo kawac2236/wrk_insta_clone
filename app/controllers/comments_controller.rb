d248caebbf7ca580d31eb870d393084935281123
kawac2236 <kawac2236@gmail.com>
2019-10-26 14:59:10 +0900

    add: コメントの追加・更新時にログインをあらかじめ要求する #8

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+  # ログインを要求するアクションの指定
+  before_action :require_login, only: %i[create update destroy]
+
   def create
     @comment = current_user.comments.build(comment_params)
     @comment.save
