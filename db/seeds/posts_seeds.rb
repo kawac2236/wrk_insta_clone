006ce3851a1f70413243a8d3e702343fe1e307ab
kawac2236 <kawac2236@gmail.com>
2019-10-26 12:47:19 +0900

    refactor: seedの中でダウンロードするようにする。 #4

--- db/seeds/posts_seeds.rb
+++ db/seeds/posts_seeds.rb
@@ -1,14 +1,9 @@
 # Post
 puts ' "Post" を作成開始します ...'
-image_id = 1
 User.limit(20).each do |user|
   post = user.posts.create(
     content:Faker::Lorem.sentence,
-    images: [
-      open("./db/fixtures/post_images/image_#{image_id.to_s}.jpg"),
-      open("./db/fixtures/post_images/image_#{(image_id + 1).to_s}.jpg")
-    ]
+    remote_images_urls: %w[https://picsum.photos/350/350/?random https://picsum.photos/350/350/?random]
   )
-  image_id += 2
   puts "post#{post.id} が作成されました"
 end
