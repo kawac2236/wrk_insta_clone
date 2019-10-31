f188d8963dd2c2d4d3e13f4382c77de2ae0796a5
kawac2236 <kawac2236@gmail.com>
2019-10-31 22:01:10 +0900

    add: いいね機能を同期処理で仮実装する #10

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,10 +2,12 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     current_user.like(@post)
+    redirect_to root_path
   end
 
   def destroy
     @post = Like.find(params[:id]).post
     current_user.unlike(@post)
+    redirect_to root_path
   end
 end
