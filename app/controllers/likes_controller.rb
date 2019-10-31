8664b94ed085922a8a153c7c8ff9ba502dfe95a8
kawac2236 <kawac2236@gmail.com>
2019-10-31 22:12:20 +0900

    feat: 非同期処理として実装 #10

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,10 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     current_user.like(@post)
-    redirect_to root_path
   end
 
   def destroy
     @post = Like.find(params[:id]).post
     current_user.unlike(@post)
-    redirect_to root_path
   end
 end
