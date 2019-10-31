baa135725f13c0229003749bdcd72b2cf2fa6f6f
kawac2236 <kawac2236@gmail.com>
2019-10-31 21:45:18 +0900

    add: Likeテーブルにユニークインデックスを追加

--- db/migrate/20191027003025_create_likes.rb
+++ db/migrate/20191027003025_create_likes.rb
@@ -5,6 +5,7 @@ class CreateLikes < ActiveRecord::Migration[5.2]
       t.references :post, foreign_key: true
 
       t.timestamps
+      t.index [:user_id, :post_id], unique: true
     end
   end
 end
