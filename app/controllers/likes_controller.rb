5327971027de499671d0b36acfad4f524a9d8a67
kawac2236 <kawac2236@gmail.com>
2019-10-31 21:54:17 +0900

    modify: controllerのメソッドの修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,11 @@
 class LikesController < ApplicationController
   def create
-    @post = Post.find_by(params[:id])
+    @post = Post.find(params[:post_id])
     current_user.like(@post)
   end
 
   def destroy
-    @post = Post.find_by(params[:id])
+    @post = Like.find(params[:id]).post
     current_user.unlike(@post)
   end
 end
