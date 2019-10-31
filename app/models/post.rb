2a15b7874384512fd6ec2011f0d02a1d042b61ab
kawac2236 <kawac2236@gmail.com>
2019-10-31 22:13:16 +0900

    add: Post -> User方向の結合モデルの追加 #10

--- app/models/post.rb
+++ app/models/post.rb
@@ -19,6 +19,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :like_users, through: :likes, source: :user
 
   validates :content, presence: true, length: { maximum: 1000 }
   validates :images, presence: true
