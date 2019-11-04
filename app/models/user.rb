5989435feda1c59d4989e783b109dfa56585c344
kawac2236 <kawac2236@gmail.com>
2019-11-04 10:56:15 +0900

    feat: follow/unfollow/follow?メソッドの追加 #12

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,4 +55,17 @@ class User < ApplicationRecord
   def like?(post)
     like_posts.include?(post)
   end
+
+  # follow/unfollow
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def follow?(other_user)
+    following.include?(other_user)
+  end
 end
