c59e8ff565714db83f2b6a27046b893bd24b6e39
kawac2236 <kawac2236@gmail.com>
2019-11-04 10:54:44 +0900

    feat: relationshipモデルの追加 #12

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,11 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   # 結合モデル
   has_many :like_posts, through: :likes, source: :post
-
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followed, through: :passive_relationships, source: :following
+  
   authenticates_with_sorcery!
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
