c42af548e6827733a6414a87a670b83cb887e474
kawac2236 <kawac2236@gmail.com>
2019-11-04 11:02:56 +0900

    feat: フォロー/アンフォローを非同期で実装 #12

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,12 +4,10 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:followed_id])
     current_user.follow(@user)
-    redirect_to users_path
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow(@user)
-    redirect_to users_path
   end
 end
