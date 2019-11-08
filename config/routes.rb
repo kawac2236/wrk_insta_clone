8377283bd23329342fa37c398fa5ac544593577d
kawac2236 <kawac2236@gmail.com>
2019-11-09 00:26:31 +0900

    modify: searchのroutesメソッドをシンボルにする #15

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :users, only: %i[show create new index]
   resources :posts, shallow: true do
     collection do
-      get 'search'
+      get :search
     end
     resources :comments
   end
