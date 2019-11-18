859cc1516669b963ff6bfca057e0d769db4ab7bd
kawac2236 <kawac2236@gmail.com>
2019-11-18 19:26:59 +0900

    feat: mypageのコントローラー #19

---
+++ app/controllers/mypage/accounts_controller.rb
@@ -0,0 +1,20 @@
+class Mypage::AccountsController < ApplicationController
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      redirect_to edit_mypage_account_path, success: '更新に成功しました'
+    else
+      flash.now['danger'] = 'プロフィールの更新に失敗しました'
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end
