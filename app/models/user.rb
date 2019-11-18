e44532e38edb73b45c4a9c8ea81247de13cd1cf2
kawac2236 <kawac2236@gmail.com>
2019-11-19 06:55:41 +0900

    feat: uploaderをavatarにマウント #19

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,8 @@
 #
 
 class User < ApplicationRecord
+  mount_uploader :avatar, AvatarUploader
+
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
