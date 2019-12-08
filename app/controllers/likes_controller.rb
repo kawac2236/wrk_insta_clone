6a1edba8d1b33977cb603fe1df635b88112b1c1d
kawac <kawac2236@gmail.com>
2019-12-08 17:44:03 +0900

    feat: いいね時の通知メールの実装 #26

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,6 +4,11 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     current_user.like(@post)
+    UserMailer.with(
+      user_from: current_user,
+      user_to: @post.user,
+      post: @post
+    ).like_post.deliver_now # if current_user.like?(@post)
   end
 
   def destroy
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,10 @@
 class UserMailer < ApplicationMailer
+	default from: 'notifications-insta-clone@example.com'
+	# 仮実装
+	def like_post
+		@post = params[:post]
+		@user_to = params[:user_to]
+		@user_from = params[:user_from]
+    mail(to: @user_to.email, subject: "#{@user_from.name}があなたの投稿にいいねしました")
+  end
 end
