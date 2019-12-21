5ec03521ed6c23797522ffed46075250cf2cddc7
kawac <kawac2236@gmail.com>
2019-12-21 23:24:51 +0900

    post_specのバリデーション

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーション" do
+    it '画像は必須であること' do
+      post = build(:post, images: nil)
+      post.valid?
+      expect(post.errors[:images]).to include('を入力してください')
+    end
+
+    it '本文は必須であること' do
+      post = build(:post, content: nil)
+      post.valid?
+      expect(post.errors[:content]).to include('を入力してください')
+    end
+
+    it '本文は最大1000文字であること' do
+      post = build(:post, content: 'a' * 1001)
+      post.valid?
+      expect(post.errors[:content]).to include('は1000文字以内で入力してください')
+    end
+
+  end
 end
