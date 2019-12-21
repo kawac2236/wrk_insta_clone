82e186d8a9914bff1b25d6fe8c7836d0f703c1e9
kawac <kawac2236@gmail.com>
2019-12-22 06:00:17 +0900

    fix: 指摘事項の対応

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,9 +1,7 @@
 FactoryBot.define do
   factory :comment do
     content { Faker::Lorem.sentence}
-    after(:build) do |comment|
-      comment.user = FactoryBot.create(:user)
-      comment.post = FactoryBot.create(:post, user: comment.user)
-    end
+    user
+    post
   end
 end
