59b4bba767aa6d25d032d5d65f8b790467dd2e36
kawac <kawac2236@gmail.com>
2019-12-21 13:28:50 +0900

    doc: userモデルのテストパターン

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+#     it 'ユーザー名は必須であること' do
+#     it 'ユーザー名は一意であること' do
+#     it 'メールアドレスは必須であること' do
+#     it 'メールアドレスは一意であること' do
+#     it '自分のオブジェクトの場合trueを返す' do
+#     it '自分のオブジェクトの場合falseを返す' do
+#     it 'いいねできること' do
+#     it 'いいねを解除できること' do
+#     it 'フォローできること' do
+#     it 'フォローを外せること' do
+#     it 'フォローしている場合trueを返す' do
+#     it 'フォローしている場合falseを返す' do
+#     it { is_expected.to include post_by_user_a }
+#     it { is_expected.to include post_by_user_b }
+#     it { is_expected.not_to include post_by_user_c }
 end
