ae407d31fd680b5697b03e24b96ecd5b51f9bc33
kawac <kawac2236@gmail.com>
2019-12-26 22:29:30 +0900

    feat: user_spec 入力に失敗した場合

--- spec/system/users/user_spec.rb
+++ spec/system/users/user_spec.rb
@@ -15,5 +15,21 @@ RSpec.describe 'ユーザー登録', type: :system do
         expect(page).to have_content '登録に成功しました'
       end
     end
+
+		context '入力情報に誤りがある場合' do
+			it 'ユーザー登録に失敗すること' do
+				visit new_user_path
+				fill_in 'ユーザー名', with: ''
+				fill_in 'メールアドレス', with: ''
+				fill_in 'パスワード', with: ''
+				fill_in 'パスワード確認', with: ''
+				click_button '登録'
+				expect(page).to have_content '登録に失敗しました'
+				expect(page).to have_content 'ユーザー名を入力してください'
+				expect(page).to have_content 'メールアドレスを入力してください'
+				expect(page).to have_content 'パスワードは6文字以上で入力してください'
+				expect(page).to have_content 'パスワード確認を入力してください'
+			end
+		end
 	end
 end
