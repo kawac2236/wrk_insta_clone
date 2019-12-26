9d4e6d038e82a19b42998744d15f1632c0a33557
kawac <kawac2236@gmail.com>
2019-12-26 22:49:50 +0900

    doc: テスト内容の書き出し

--- spec/system/posts/post_spec.rb
+++ spec/system/posts/post_spec.rb
@@ -7,6 +7,9 @@ RSpec.describe 'ポスト', type: :system do
 		let!(:post_1_by_others) { create(:post) }
 		let!(:post_2_by_others) { create(:post) }
 		let!(:post_by_user) { create(:post, user: user) }
+
+		# ログインしている場合
+		# 	フォロワーと自分の投稿だけが表示されること
 		context 'ログインしていない場合' do
 			it 'すべてのポストが表示されること' do
 				visit posts_path
@@ -15,5 +18,30 @@ RSpec.describe 'ポスト', type: :system do
 				expect(page).to have_content post_by_user.content
 			end
 		end
+	end
+
+	describe 'ポスト投稿' do
+		# 画像投稿できること
+	end
+
+	describe 'ポスト更新' do
+		# 自分の投稿に編集ポタンが表示されること
+		# 痰飲の投稿には編集ポタンが表示されないこと
+		# 投稿が更新できること
+	end
+
+	describe 'ポスト削除' do
+		# 自分の投稿に削除ボタンが表示されること
+		# 他人の投稿に削除ボタンが表示されないこと
+		# 投稿がさくじょできること
+	end
+
+	describe 'ポスト詳細' do
+		# 投稿の詳細画面が閲覧できること
+	end
+
+	describe 'いいね' do
+		# いいねができること
+		# いいねが取り消せること
 	end
 end
